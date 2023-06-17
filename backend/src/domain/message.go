145bbd0b13418d87630f0ef842cdaf66e91e8b2a
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2023-06-17 09:18:11 +0900

    タグ追加

--- backend/src/domain/message.go
+++ backend/src/domain/message.go
@@ -7,12 +7,12 @@ import (
 )
 
 type Message struct {
-	User      string
-	MessageId uuid.UUID
-	Channel   string
-	Content   string
-	CreatedAt time.Time
-	Ika       bool
-	Shika     bool
-	Meka      bool
+	User      string    `json:"user,omitempty" db:"user"`
+	MessageId uuid.UUID `json:"messageId,omitempty" db:"message_id"`
+	Channel   string    `json:"channel,omitempty" db:"channel"`
+	Content   string    `json:"content,omitempty" db:"content"`
+	CreatedAt time.Time `json:"createdAt,omitempty" db:"created_at"`
+	Ika       bool      `json:"ika,omitempty" db:"ika"`
+	Shika     bool      `json:"shika,omitempty" db:"shika"`
+	Meka      bool      `json:"meka,omitempty" db:"meka"`
 }
