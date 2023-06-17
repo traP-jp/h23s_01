0b5e15c5a4b44358de47e8c9ef5b7fe8ff6182ee
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2023-06-18 00:32:16 +0900

    originの環境変数

--- backend/src/config/origin.go
+++ backend/src/config/origin.go
@@ -5,7 +5,7 @@ import "os"
 func GetAccessControlAllowOrigin() string {
 	origin, ok := os.LookupEnv("ACCESS_CONTROL_ALLOW_ORIGIN")
 	if !ok {
-		return "*"
+		return "http://localhost:5173"
 	}
 	return origin
 }
