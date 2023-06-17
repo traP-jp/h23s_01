082cf818aa7b5598ba1f38b6bd799ee8ed8c613e
aya-se <kakkeno1@gmail.com>
2023-06-17 19:01:53 +0900

    認証の仮作成

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,13 @@
 <script setup>
-import { status, allList, leftList, middleList, rightList } from "./store.js";
+import {
+  status,
+  allList,
+  leftList,
+  middleList,
+  rightList,
+  user_id,
+  user_name,
+} from "./store.js";
 import { onMounted } from "vue";
 import Title from "./pages/Title.vue";
 import Game from "./pages/Game.vue";
@@ -7,9 +15,49 @@ import Result from "./pages/Result.vue";
 import axios from "axios";
 
 onMounted(() => {
-  const searchParams = new URLSearchParams( window.location.search );
+  const searchParams = new URLSearchParams(window.location.search);
   if (searchParams.toString() !== "") {
-    axios.get("http://localhost:8080/api/oauth2/callback?"+searchParams);
+    axios
+      .get("http://localhost:8080/api/me", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log(res.data);
+        user_id.value = res.data.id;
+        user_name.value = res.data.name;
+      })
+      .catch(() => {
+        axios
+          .get("http://localhost:8080/api/oauth2/callback?" + searchParams, {
+            withCredentials: true,
+          })
+          .then(
+            axios
+              .get("http://localhost:8080/api/me", {
+                withCredentials: true,
+              })
+              .then((res) => {
+                console.log(res.data);
+                user_id.value = res.data.id;
+                user_name.value = res.data.name;
+              })
+          );
+      });
+  } else {
+    axios
+      .get("http://localhost:8080/api/me", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log(res.data);
+        user_id.value = res.data.id;
+        user_name.value = res.data.name;
+      })
+      .catch(
+        axios.get("http://localhost:8080/api/oauth2/authorize", {
+          withCredentials: true,
+        })
+      );
   }
   for (let i = 1; i < 100; i++) {
     allList.value.push({
@@ -33,31 +81,28 @@ onMounted(() => {
     rightList.value.push(allList.value[i]);
   }
 });
-
-
-
-const onClickButton = () => {
-  axios.get("http://localhost:8080/api/oauth2/callback?"+searchParams);
-};
 </script>
 
 <template>
   <div>
-    <select v-model="status">
-      <option value="title">Title</option>
-      <option value="game">Game</option>
-      <option value="result">Result</option>
-    </select>
-    <a href="http://localhost:8080/api/oauth2/authorize">認証用リンク</a>
-    <button @click="onClickButton">sdfgsdfg</button>
-    <div v-if="status === 'title'">
-      <Title />
+    <div v-if="!user_id">
+      <a href="http://localhost:8080/api/oauth2/authorize">認証用リンク</a>
     </div>
-    <div v-if="status === 'game'">
-      <Game />
-    </div>
-    <div v-if="status === 'result'">
-      <Result />
+    <div v-else>
+      <select v-model="status">
+        <option value="title">Title</option>
+        <option value="game">Game</option>
+        <option value="result">Result</option>
+      </select>
+      <div v-if="status === 'title'">
+        <Title />
+      </div>
+      <div v-if="status === 'game'">
+        <Game />
+      </div>
+      <div v-if="status === 'result'">
+        <Result />
+      </div>
     </div>
   </div>
 </template>
