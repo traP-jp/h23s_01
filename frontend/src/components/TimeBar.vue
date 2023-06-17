93464bc9047704dd700a70564141792b5824376c
Akria <iwabuchi.akira.2003@gmail.com>
2023-06-17 19:12:04 +0900

    たいむ婆

--- frontend/src/components/TimeBar.vue
+++ frontend/src/components/TimeBar.vue
@@ -3,5 +3,26 @@ import { gameTimer } from "../store";
 </script>
 
 <template>
-    <p>{{ gameTimer }}</p>
+    <div style="display: flex">
+        <h2 style="margin: auto;">{{ gameTimer }}</h2>
+        <div class="remaining-bar">
+            <div class="remaining" :style="`width: ${gameTimer / 30 * 100}%`"></div>
+        </div>
+    </div>
 </template>
+
+<style>
+.remaining-bar {
+    width: 80%;
+    height: 20px;
+    margin: auto;
+    background-color: lightgray;
+    border-radius: 10px;
+    overflow: hidden;
+}
+
+.remaining {
+    height: 100%;
+    background-color: #646fcc;
+}
+</style>
