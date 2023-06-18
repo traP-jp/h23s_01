ffc9e6def706b1a7c2b1a5e270f1335bef937af7
mathsuky <yuida2718@gmail.com>
2023-06-18 15:57:16 +0900

    遊び方追加(最新)

--- frontend/src/pages/Title.vue
+++ frontend/src/pages/Title.vue
@@ -21,6 +21,21 @@ const play = () => {
       <div class="user_name">{{ user_name }}</div>
     </div>
     <button @click="play" class="play">あそぶ</button>
+    <div class="howtoplay">
+      <h2>遊び方</h2>
+      <div class="howtoplaytext">
+        <p>
+          1.一定の間隔でtraQのgps/times以下のイカした投稿が流れてきます。<br />
+          2.流れてくる投稿のうち，<hilight>「いか」「しか」「めか」</hilight>のどれかが含まれた投稿を見つけて選択すればポイントが得られます。<br />(ひらがな，カタカタ，漢字が対象です。例えば「
+          医科歯科」，「いい感じ」，「イカしてる」などです。)<br />
+          3.「いか」「しか」「めか」のどれも含まれない投稿を選択すると，<hilight>3秒間投稿を選択することができなく</hilight>なります。<br />
+          4.制限時間が終了するとリザルト画面に移動し，スコアと「いか」「しか」「めか」が含まれた投稿の一覧が表示されます。<br />
+          <hilight2
+            >リザルト画面からtraQの投稿に飛ぶことができるので，素敵な投稿を見つけたら是非timesに遊びに行きましょう!</hilight2
+          >
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -54,4 +69,25 @@ const play = () => {
     font-weight: bold;
   }
 }
+  .howtoplay {
+    padding: 10px;
+    padding-top: 0px;
+    border: solid 3px #646fcc;
+    border-radius: 10px;
+    margin: 30px;
+    width: 895px;
+  }
+  .howtoplaytext {
+    text-align: left;
+    line-height: 30px;
+  }
+  hilight {
+    color: #f01d2e;
+    font-weight: bold;
+  }
+  hilight2 {
+    color: #f5b847;
+    font-weight: bold;
+  }
+
 </style>
